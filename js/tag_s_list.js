b0185e412dc36275b44f174c123fde26c5b4f693
obarads <b1510803@planet.kanazawa-it.ac.jp>
2019-05-03 16:17:16 +0900

    update

--- js/tag_s_list.js
+++ js/tag_s_list.js
@@ -1,3 +1,3 @@
 function tag_task_list(){ return ['3D Estimation','Adversarial Examples','Analytics','Classification','Data Augmentation','Dataset','Detection','Domain Adaptation','Few-Shot','Flow Estimation','Gaze Estimation','Instance Segmentation','One-Class & Anomaly Detection','Registration','SLAM','Semantic Segmentation']}
 function tag_data_list(){ return ['2D Image','Mesh','Point Cloud','Video','Voxel']}
-function tag_etc_list(){ return ['AutoEncoder','GAN','Graph','RNN','Self-Supervision','Sparsity','Survey','Synthetic','TreeRNN']}
+function tag_etc_list(){ return ['AutoEncoder','GAN','Graph','RNN','Reinforcement','Self-Supervision','Sparsity','Survey','Synthetic','TreeRNN']}
---
+++ papers/Improving Spatiotemporal Self-Supervision by Deep Reinforcement Learning.md
@@ -0,0 +1,37 @@
+# Improving Spatiotemporal Self-Supervision by Deep Reinforcement Learning
+
+元の論文の公開ページ : https://arxiv.org/abs/1807.11293
+
+## どんなもの?
+
+## 先行研究と比べてどこがすごいの?
+
+## 技術や手法のキモはどこ? or 提案手法の詳細
+
+## どうやって有効だと検証した?
+
+## 議論はある?
+
+## 次に読むべき論文は?
+- なし
+
+## 論文関連リンク
+1. なし
+
+## 会議
+ECCV 2018
+
+## 著者
+Uta Büchler, Biagio Brattoli, Björn Ommer.
+
+## 投稿日付(yyyy/MM/dd)
+2018/07/30
+
+## コメント
+なし
+
+## key-words
+Reinforcement, Self-Supervision
+
+## status
+省略
